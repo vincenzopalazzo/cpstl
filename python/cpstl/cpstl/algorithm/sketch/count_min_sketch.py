c86c5ec52870ef25142aaea9ba5a587fb5069b4e
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-03-29 19:41:42 +0200

    python: add count min sketch
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

---
+++ python/cpstl/cpstl/algorithm/sketch/count_min_sketch.py
@@ -0,0 +1,84 @@
+"""
+Count Min Sketch algorithm in pure python 3.
+Copyright (C) 2021-2022 Vincenzo Palazzo vincenzopalazzodev@gmail.com
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+"""
+from array import array
+from math import pow
+from ..hash.universal_hash import UniversalHash
+
+
+def generate_n_hash_function(
+    number_hashes: int, universe_hash: int = int(pow(2, 32))
+) -> list:
+    """
+    Generate N hash functions!
+    :param universe_hash: The size of the universe of the hash functions
+    :param number_hashes: Number of hash functions!
+    :return: return the list of hash function
+    """
+    hashes = []
+    for _ in range(number_hashes):
+        hashes.append(UniversalHash(size=universe_hash))
+    return hashes
+
+
+class CountMinSketch:
+    """
+    Count Min Sketch algorithm in pure python 3.
+
+    author: Vincenzo Palazzo https://github.com/vincenzopalazzo
+    """
+
+    def __init__(self, size_hash_table: int, hash_functions: int):
+        self.size = 0
+        self.matrix = []
+        # TODO: how the matrix is defined?
+        for _ in range(size_hash_table):
+            table = array("l", (0 for _ in range(hash_functions)))
+            self.matrix.append(table)
+
+        self.hash_functions = generate_n_hash_function(
+            number_hashes=hash_functions, universe_hash=size_hash_table
+        )
+
+    def __compute_hashes(self, val):
+        if not isinstance(val, int):
+            raise Exception(
+                "Value not an instance of int, other type are not supported by the Universal Hash function!"
+            )
+        for idx in range(len(self.hash_functions)):
+            yield self.hash_functions[idx].hash(val)
+
+    def increment(self, value, increment: int = 1) -> None:
+        """Apply an increment to the value"""
+        self.size += increment
+        for table, i in zip(self.matrix, self.__compute_hashes(value)):
+            table[i] += increment
+
+    def query(self, value) -> int:
+        """Query the data structure and return the estimation frequency of the value"""
+        print(self.matrix)
+        return min(
+            table[i] for table, i in zip(self.matrix, self.__compute_hashes(value))
+        )
+
+    def __getitem__(self, item):
+        return self.query(item)
+
+    def __len__(self):
+        return self.size
